a1cd0d889efdadcad74f085397be2d4ba5a7afca
nataliemars30-tech <nataliemars30@gmail.com>
2026-06-20 14:29:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
+DATABASE_PATH = os.environ.get("DATABASE_PATH", "ren_bot.db")
+TIMEZONE = os.environ.get("TIMEZONE", "Europe/Moscow")
+
+REGISTER_NAME = 0
+BOUQUET_PHOTO = 1
+BOUQUET_PRICE = 2
+TASK_PHOTO = 3
+TASK_REASON = 4
+SETTINGS_CHOOSE = 5
+SETTINGS_VALUE = 6
+FLOWWOW_PHOTO = 7import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 DIRECTOR_ID = int(os.getenv("DIRECTOR_ID", "0"))
